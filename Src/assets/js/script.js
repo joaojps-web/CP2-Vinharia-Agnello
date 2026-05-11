74adb283d396cf4370427a783d0af0bb360b6206
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-11 11:12:02 -0300

    Feat: correcao codigo JS

--- Src/assets/js/script.js
+++ Src/assets/js/script.js
@@ -49,64 +49,64 @@ function Iniciar(){
     let nome5, ano5, quant5, class5;
 
     let anoAtualDigitado = prompt("Digite o ano atual:");
+    if (!validarTexto(anoAtualDigitado)) return;
 
     let continuar = "s";
 
     while (continuar === "s") {
 
         let nome = prompt("Digite o nome do vinho:");
+        if (!validarTexto(nome)) return;
+
         let anoDigitado = prompt("Digite o ano do vinho:");
+        if (!validarTexto(anoDigitado)) return;
+
         let quantidade = prompt("Digite a quantidade em estoque:");
+        if (!validarTexto(quantidade)) return;
 
-        if (validarTexto(nome) && validarTexto(anoDigitado) && validarTexto(quantidade)) {
+        let anoNum = parseInt(anoDigitado);
+        let quantNum = parseInt(quantidade);
+        let anoAtualNum = parseInt(anoAtualDigitado);
+        let classificacao = classificarVinho(anoNum, anoAtualNum);
 
-            let anoNum = parseInt(anoDigitado);
-            let quantNum = parseInt(quantidade);
-            let anoAtualNum = parseInt(anoAtualDigitado);
-            let classificacao = classificarVinho(anoNum, anoAtualNum);
+        totalCadastros = totalCadastros + 1;
 
-            totalCadastros = totalCadastros + 1;
-
-            if (estoqueEstaBaixo(quantNum)) {
-                totalEstoqueBaixo = totalEstoqueBaixo + 1;
-            }
-
-            if (anoNum < vinhoMaisAntigoAno) {
-                vinhoMaisAntigoAno = anoNum;
-                vinhoMaisAntigoNome = nome;
-            }
-
-            if (totalCadastros === 1) {
-                nome1 = nome; ano1 = anoNum; quant1 = quantNum; class1 = classificacao;
-                exibirDadosVinho(nome1, ano1, quant1, class1);
-            } else if (totalCadastros === 2) {
-                nome2 = nome; ano2 = anoNum; quant2 = quantNum; class2 = classificacao;
-                exibirDadosVinho(nome2, ano2, quant2, class2);
-            } else if (totalCadastros === 3) {
-                nome3 = nome; ano3 = anoNum; quant3 = quantNum; class3 = classificacao;
-                exibirDadosVinho(nome3, ano3, quant3, class3);
-            } else if (totalCadastros === 4) {
-                nome4 = nome; ano4 = anoNum; quant4 = quantNum; class4 = classificacao;
-                exibirDadosVinho(nome4, ano4, quant4, class4);
-            } else if (totalCadastros === 5) {
-                nome5 = nome; ano5 = anoNum; quant5 = quantNum; class5 = classificacao;
-                exibirDadosVinho(nome5, ano5, quant5, class5);
-            }
-
-            alert("Cadastro realizado!");
-
-            continuar = prompt("Deseja cadastrar outro vinho? (s/n)");
-            if (continuar === null) {
-                continuar = "n";
-            }
-            continuar = continuar.toLowerCase();
-
+        if (estoqueEstaBaixo(quantNum)) {
+            totalEstoqueBaixo = totalEstoqueBaixo + 1;
         }
+
+        if (anoNum < vinhoMaisAntigoAno) {
+            vinhoMaisAntigoAno = anoNum;
+            vinhoMaisAntigoNome = nome;
+        }
+
+        if (totalCadastros === 1) {
+            nome1 = nome; ano1 = anoNum; quant1 = quantNum; class1 = classificacao;
+            exibirDadosVinho(nome1, ano1, quant1, class1);
+        } else if (totalCadastros === 2) {
+            nome2 = nome; ano2 = anoNum; quant2 = quantNum; class2 = classificacao;
+            exibirDadosVinho(nome2, ano2, quant2, class2);
+        } else if (totalCadastros === 3) {
+            nome3 = nome; ano3 = anoNum; quant3 = quantNum; class3 = classificacao;
+            exibirDadosVinho(nome3, ano3, quant3, class3);
+        } else if (totalCadastros === 4) {
+            nome4 = nome; ano4 = anoNum; quant4 = quantNum; class4 = classificacao;
+            exibirDadosVinho(nome4, ano4, quant4, class4);
+        } else if (totalCadastros === 5) {
+            nome5 = nome; ano5 = anoNum; quant5 = quantNum; class5 = classificacao;
+            exibirDadosVinho(nome5, ano5, quant5, class5);
+        }
+
+        alert("Cadastro realizado!");
+
+        continuar = prompt("Deseja cadastrar outro vinho? (s/n)");
+        if (!validarTexto(continuar)) return;
+        continuar = continuar.toLowerCase();
     }
 
-    let relatorio = "RELATÓRIO FINAL ";
-    relatorio += `Total de cadastros feitos: ${totalCadastros}`;
-    relatorio += `Vinhos com estoque baixo: ${totalEstoqueBaixo}`;
+    let relatorio = "RELATÓRIO FINAL\n";
+    relatorio += `Total de cadastros feitos: ${totalCadastros}\n`;
+    relatorio += `Vinhos com estoque baixo: ${totalEstoqueBaixo}\n`;
     relatorio += `Vinho com safra mais antiga: ${vinhoMaisAntigoNome} (${vinhoMaisAntigoAno})`;
 
     console.log(relatorio);
